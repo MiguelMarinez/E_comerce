1bdab35b621e9dde055790b51b35596e4921488e
Miguel Marinez <Chamo7110@gmail.com>
2024-07-27 15:18:15 -0400

    first commit

---
+++ src/graficos.py
@@ -0,0 +1,246 @@
+import pandas as pd
+import plotly.express as px 
+import plotly.graph_objects as go
+import json
+import requests
+
+
+def pregunta_1(df):
+    
+    
+    top_productos = df.groupby('tipo_producto')['cantidad'].sum().sort_values(ascending=False).head(5)
+    top_productos = top_productos.reset_index()
+    
+  
+    
+    fig_pizza = px.pie(top_productos, 
+                   values='cantidad', 
+                   names='tipo_producto', 
+                   title='Top 5 Productos Más Vendidos Históricamente',
+                   color='tipo_producto',
+                   color_discrete_sequence=px.colors.sequential.Blues_r  # Usa una escala de azules
+                   )
+
+    fig_pizza.update_layout(
+        title={'text':'5 Productos Más Vendidos Históricamente',
+               'y':0.90, 'x':0.5, 'xanchor': 'center', 'yanchor': 'top'},
+        #width = 400,
+        #height=350,
+        template='plotly_dark',
+        font=dict(
+            family='Arial, sans-serif',
+            size=10,
+            color='white'
+        ),
+        showlegend=False,
+        margin=dict(l=0, r=0, t=70, b=20)
+    )
+
+    fig_pizza.update_traces(textposition='inside', textinfo='percent+label', textfont=dict(color='white'))
+    
+    fig_pizza.update_traces(hovertemplate="<br><span style='color: #ffffff;'>Cantidad: $%{value:,.0f}<extra></extra>")
+    
+    return fig_pizza
+
+
+
+
+def pregunta_2(df):
+    
+    
+    
+    df['ingresos_netos'] = df['valor_total'] - df['costo_envio']
+    ingresos_historico = df.groupby(['año_compra', 'mes_compra'])['ingresos_netos'].sum().reset_index()
+    
+    #ingresos_historico.head()
+    
+    # para mapear números de mes a nombres de mes
+    month_names = {1: 'Enero', 2: 'Febrero', 3: 'Marzo', 4: 'Abril', 5: 'Mayo', 6: 'Junio', 
+               7: 'Julio', 8: 'Agosto', 9: 'Septiembre', 10: 'Octubre', 11: 'Noviembre', 12: 'Diciembre'}
+
+
+    fig = px.line(ingresos_historico, x='mes_compra', y='ingresos_netos', color='año_compra', markers=True,
+                labels={'ingresos_netos': 'Ingresos Netos', 'mes_compra': 'Mes', 'año_compra': 'Año'},
+                title='Evolución Histórica de Ingresos Netos por Año',
+                hover_data={'ingresos_netos': ':.2f'})
+
+    fig.update_layout(
+        xaxis_title='Mes',
+        yaxis_title='Ingresos Netos',
+        legend_title_text='Año',
+        template='plotly_dark', 
+        xaxis=dict(
+            tickmode='array',
+            tickvals=list(month_names.keys()),
+            ticktext=list(month_names.values()),
+            tickangle=45
+        )
+    )
+
+    #fig.update_traces(line=dict(color='blue'), )
+    #fig.update_traces(line=dict(color='red'), selector=dict(name='2020'))
+    #fig.update_traces(line=dict(color='green'),selector=dict(name='2021'))
+
+    color_map = {2019: 'blue', 2020: 'red', 2021: 'green'}
+    for year, color in color_map.items():
+        fig.update_traces(
+            line=dict(color=color),
+            selector=dict(name=str(year)),
+            hovertemplate=f'Año: {year}<br>Mes: %{{x}}<br>Ingresos Netos: %{{y:$,.2f}}<extra></extra>')
+
+
+    return fig
+
+
+
+
+def pregunta_3(df):
+    
+    
+    ingresos_por_vendedor  = df.groupby(['nombre_vendedor','año_compra'])['ingresos_netos'].sum().reset_index()
+    ingresos_por_vendedor = ingresos_por_vendedor[ingresos_por_vendedor['nombre_vendedor']!= 'Unknown']
+    ingresos_por_vendedor = ingresos_por_vendedor.sort_values(by=['año_compra', 'nombre_vendedor'])
+  
+    
+    
+    # Crear la figura
+    fig = go.Figure()
+
+    # Agregar las barras apiladas para cada año
+    for year in ingresos_por_vendedor['año_compra'].unique():
+        df_year = ingresos_por_vendedor[ingresos_por_vendedor['año_compra'] == year]
+        fig.add_trace(go.Bar(
+            x=df_year['nombre_vendedor'],
+            y=df_year['ingresos_netos'],
+            name=str(year)
+        ))
+
+    # Configurar el diseño del gráfico
+    fig.update_layout(
+        barmode='group',
+        title='Ingresos Netos por Vendedor y Año',
+        xaxis=dict(title='Vendedor'),
+        yaxis=dict(title='Ingresos Netos'),
+        legend=dict(title='Año'),
+        template='plotly_dark',
+    )
+
+    fig.update_traces(hovertemplate="<br><span style='color: #ffffff;'>Cantidad: $%{value:,.0f}<extra></extra>")
+
+    return fig
+
+
+
+
+
+def pregunta_4(df):
+    
+    
+    
+    # df_final.head(1)
+    
+    ingresos_por_ciudad = df.groupby('ciudad_nombre')['ingresos_netos'].sum().reset_index()
+    ingresos_por_ciudad = ingresos_por_ciudad.sort_values(by='ingresos_netos', ascending=False)
+    
+    
+    # ingresos_por_ciudad.head(5)
+    
+    # Descargar el archivo GeoJSON de Brasil
+    url = 'https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson'
+    response = requests.get(url)
+    brasil_geojson = response.json()
+    
+    
+    
+    
+        # Crear el mapa base con las coordenadas de Brasil y las ciudades
+    fig = px.choropleth_mapbox(ingresos_por_ciudad,
+                            geojson=brasil_geojson,
+                            locations='ciudad_nombre',
+                            featureidkey="properties.name",
+                            color='ingresos_netos',
+                            color_continuous_scale='Blues',
+                            hover_name='ciudad_nombre',
+                            custom_data=['ciudad_nombre','ingresos_netos' ],
+                            center={"lat": -14.2350, "lon": -51.9253},
+                            mapbox_style="carto-positron",
+                            zoom=2.5,
+                            title='Ingresos por Estados en Brasil')
+
+
+
+    fig.update_layout(#width=600, height=500,
+        paper_bgcolor='rgb(0,0,0,0)', 
+        plot_bgcolor='rgb(0,0,0,0)',
+        title_x=0.12,
+        margin={'r':0,'t':70,'l':5,'b':0},
+        mapbox=dict(center=dict(lat=-14.2350, lon=-51.9253),
+                layers=[
+            
+                {
+                    "source": brasil_geojson,
+                    "type": "fill",
+                    "below": "traces",
+                    "color": "rgba(0, 0, 0, 0)"  # Hacer el área de Brasil transparente
+                },
+                {
+                    "source": {
+                        "type": "FeatureCollection",
+                        "features": [
+                        {
+                            "type": "Feature",
+                            "geometry": {
+                                "type": "Polygon",
+                                "coordinates": [[[-180, -90], [180, -90], [180, 90], [-180, 90], [-180, -90]]]
+                            }
+                        }
+                    ]
+                    },
+                        "type": "fill",
+                        "below": "traces",
+                        "color": "rgb(14,17,23)"  # Fondo negro para áreas fuera de Brasil
+                }
+                    ]
+                
+    ),
+    
+    
+    
+        coloraxis_colorbar=dict(
+        title='Ingresos$',),
+        font=dict(
+            family="Arial",  
+            size=12, 
+            color="white")  
+    )  
+    
+    
+
+
+    fig.update_traces(hovertemplate="<b>Ciudad: %{customdata[0]}</b><br>Ingresos Totales : $%{customdata[1]:,.2f}<extra></extra>", 
+                  marker_line_width=0)
+
+    return fig
+
+
+
+def grafico_d(df):
+    
+    
+    total_ingesos = df['ingresos_netos'].sum()
+    df_ingresos = pd.DataFrame({'labels': ['total_ingesos'], 'ingresos_netos': [total_ingesos]})
+    
+    fig = px.pie(df_ingresos, values='ingresos_netos', names='labels',hole=0.5)
+    
+    fig.update_traces(textinfo='none', marker=dict(colors=['green']))
+    fig.update_layout(showlegend=False,
+                      annotations=[dict(text=f"{total_ingesos:,}", x=0.5, y=0.5, font_size=20, showarrow=False)])
+    
+    return fig
+    
+    
+
+
+    
+    
+    
---
+++ src/tienda_ventas.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import streamlit as st
+import graficos as graf
+
+st.set_page_config(layout= 'wide')
+
+# CSS 
+#def load_css(file):
+    #with open(file) as f:
+        #st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+#load_css('style.css')
+
+
+st.title('Análisis de Ventas y Rendimiento 📈')
+
+
+st.sidebar.image(r'C:\Users\Chamo\Desktop\Python\E_comerce\src\Python_PNG.png')
+
+
+# habrir df y darle formato a la columna de tiempo
+df_final = pd.read_csv(r'C:\Users\Chamo\Desktop\Python\E_comerce\src\df_final.csv', sep=',')
+df_final['fecha_compra'] = pd.to_datetime(df_final['fecha_compra'], errors='coerce')
+df_final['año_compra'] = df_final['año_compra'].astype(str)
+
+
+
+# filtro para las CIUDADES
+estados = sorted(list(df_final['ciudad_nombre'].unique()))
+estado = st.sidebar.multiselect("Estates", estados)
+
+if estado: 
+    df_final = df_final[df_final['ciudad_nombre'].isin(estado)]
+
+# filtro para el tipo de PRODUCTO
+productos = sorted(list(df_final['tipo_producto'].unique()))
+productos.insert(0,'All Products')
+producto = st.sidebar.selectbox('Select Produc', productos)
+
+if producto != 'All Products':
+    df_final = df_final[df_final['tipo_producto'] == producto]
+
+
+# filtro para los AÑOS
+años = sorted(list(df_final['año_compra'].unique()))
+año = st.sidebar.multiselect('Years', años)
+
+if año:
+    df_final = df_final[df_final['año_compra'].isin(año)]
+    
+
+# LLAMAR LOS GRAFICOS
+grafico_linea = graf.pregunta_2(df_final)
+grafico_barra = graf.pregunta_3(df_final)
+grafico_mapa = graf.pregunta_4(df_final)
+grafico_pizza = graf.pregunta_1(df_final)
+grafico_d = graf.grafico_d(df_final)
+
+
+# CREANDO 2 COLUMNAS y mastral grafici dentro de las columnas 
+col1, col2 = st.columns([2, 1])
+with col1:
+    #st.plotly_chart(grafico_d, use_container_width=True)
+    st.plotly_chart(grafico_mapa, use_container_width=True)
+with col2:
+    st.plotly_chart(grafico_pizza, use_container_width=True)
+    
+
+#MOSTRAL LOS GRAFICOS inferiores que no estan dentro de las columnas.
+st.plotly_chart(grafico_linea, use_container_width=True)
+st.plotly_chart(grafico_barra, use_container_width=True)
